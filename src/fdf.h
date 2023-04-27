a367caa256a629263bf612ccbfe4e0686d30e2a5
Gaetan <gaetan.marchale@hotmail.com>
2023-04-27 17:35:51 +0200

    parsing started

--- src/fdf.h
+++ src/fdf.h
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 11:26:27 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/04/25 18:41:44 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/04/27 16:22:45 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,35 +54,17 @@ typedef struct s_data
 
 typedef struct s_dot
 {
-	float	x;
-	float	y;
-	float	z;
+	int		x;
+	int		y;
+	int		z;
 	int		color;
 }	t_dot;
 
-typedef struct s_matrix
-{
-	t_dot	**mtx;
-	int		w;
-	int		h;
-	float	angle;
-}	t_matrix;
-
-typedef struct s_fdf_map
-{
-	int	**parsed_map;
-	int	row_len;
-	int	column_len;
-}	t_fdf_map;
-
 typedef struct s_map
 {
-	//t_vertice	*vert;
-	//t_face		*faces;
-	//t_vertice	*vert_last;
-	//t_face		*faces_last;
-	int			width;
-	int			height;
+	int		width;
+	int		height;
+	double	angle;
 }				t_map;
 
 void	draw_line(t_data *img, int x1, int y1, int x2, int y2);
--- src/main.c
+++ src/main.c
@@ -5,20 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/25 18:46:29 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/04/25 18:49:34 by gmarchal         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 11:54:12 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/04/25 18:45:56 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/04/27 17:06:09 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,28 +42,6 @@ t_map	*get_map(char	*path)
 	return (res);
 }
 
-
-static int	get_dimensions(t_matrix *matrix, char *path_file)
-{
-	int w;
-	int fd;
-
-	w = 0;
-	fd = 0;
-	matrix->angle = 0.785398; // pq???
-	fd = open(path_file, O_RDONLY);
-	if (fd < 0)
-		return(ft_printf("ERROR : failed to open file"));
-	matrix->w = get_width(w, fd);
-	close(fd);
-	if (matrix->w <= 0)
-		return (1);
-	get_height(&matrix->h, path_file);
-	if (matrix->h <= 0)
-		return (1);
-	return (0);
-}
-
 int main(int argc, char **argv)
 {
 	t_matrix *matrix;
@@ -90,40 +56,141 @@ int main(int argc, char **argv)
 
 */
 
-int	free_tab(char **tab)
+void	free_tab(char **tab)
 {
 	int	i;
-	int	c;
 
 	i = 0;
-	c = 0;
 	while (tab[i])
 	{
-		if (*tab[i] != '\n')
-			c++;
-		free(tab[i++]);
+		free(tab[i]);
+		i++;
 	}
 	free(tab);
-	return (c);
 }
 
+//PARSING
 
+int	get_height(char *path_file)
+{
+	int		count;
+	int		fd;
+	char	*tmp;
+
+	fd = open(path_file, O_RDONLY);
+	if (fd == -1)
+		return (-1); // to do : gerer error
+	count = 0;
+	while (1)
+	{
+		tmp = get_next_line(fd);
+		if (!tmp && count == 0)
+			return (-1); // to do : gerer error
+		if (!tmp)
+			break ;
+		count++;
+		free(tmp);
+	}
+	close(fd);
+	return (count);
+}
+
+int	get_width(char *path_file)
+{
+	int	i;
+	int	fd;
+	char	*line;
+	char	**tab;
+
+	fd = open(path_file, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	line = get_next_line(fd);
+	tab = ft_split(line, ' ');
+	free(line);
+	i = 0;
+	while (tab[i])
+		i++;
+	free_tab(tab);
+	return (i);
+}
+
+int	check_line(char *line)
+{
+	int	i;
+
+	i = 0;
+	while(line[i])
+	{
+		if (ft_isdigit(line[i]) || line[i] == ' ' || line[i] == '\n')
+			i++;
+		else if ((line[i] == '+' || line[i] == '-') && ft_isdigit(line[i + 1]))
+		{
+			i++;
+		}
+		else
+		{
+			ft_printf("map unvalid: line[i] == %c\n", line[i]);
+			return (-1);
+		}
+	}
+	return (0);
+}
+
+int	check_map(char *path_file, t_map *map)
+{
+	int	fd;
+	char *line;
+
+	fd = open(path_file, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (check_line(line) == -1)
+			return (-1);
+		line = get_next_line(fd);
+	}
+	map->height = get_height(path_file);
+	map->width = get_width(path_file);
+	return (0);
+}
+
+void	arg_error(int argc, char **argv)
+{
+	int	len;
+
+	if (argc != 2)
+	{
+		ft_printf("[ERROR]\nWrong number of arguments\n");
+		exit(EXIT_FAILURE);
+	}
+	len = (int)ft_strlen(argv[1]);
+	if (ft_strncmp(&argv[1][len - 4], ".fdf", 4) != 0)
+	{
+		ft_printf("[ERROR]\nMap is not .fdf\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	//t_fdf_map	map;
+	t_map	map;
 
 	double	x1 = 0;
 	double	y1 = 0;
 	double	x2 = 550;
 	double	y2 = 250;
-
 	(void)argc;
 	(void)argv;
-	mlx = mlx_init();
+
+	arg_error(argc, argv);
+	check_map(argv[1], &map);
+	mlx = mlx_init(); //connecte au serveur graphique
 	mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world!");
 	img.img = mlx_new_image(mlx, 1000, 1000);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
